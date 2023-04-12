08f928ac30c4981aa35e79a256334aaa5b395dfb
olegrudwit <olegrudwit@gmail.com>
2023-04-12 19:47:15 +0300

    Initial commit

---
+++ src/main/java/com/orudoi/articles_demo/ArticlesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.orudoi.articles_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesDemoApplication.class, args);
+	}
+
+}
